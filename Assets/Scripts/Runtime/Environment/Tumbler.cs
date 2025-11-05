4d51254f8a7b11e53d48365c61a1ed825aae713f
Kristoffer Jakobsson <kristoffer.tatel.jakobsson@gmail.com>
2025-11-05 10:38:04 +0100

    add asteroids

---
+++ Assets/Scripts/Runtime/Environment/Tumbler.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace NewKris.Runtime.Environment {
+    public class Tumbler : MonoBehaviour {
+        public float maxTumbleSpeed;
+        public float minTumbleSpeed;
+        public Transform pivot;
+
+        private float _tumbleSpeed;
+        private Vector3 _rotateAxis;
+
+        private void OnEnable() {
+            _tumbleSpeed = Random.Range(minTumbleSpeed, maxTumbleSpeed);
+            _rotateAxis = Random.insideUnitSphere;
+        }
+
+        private void Update() {
+            pivot.Rotate(_rotateAxis, _tumbleSpeed * Time.deltaTime);
+        }
+    }
+}
